ec2051a0593fbe05e706a9f6116d04fc2c251483
Matt <matthewbertweb@gmail.com>
2020-07-06 14:45:26 -0400

    I set up the graphql for the gatsby-image to be used for the hero image section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,20 @@ import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
+// step 1 to get the graphql set up below, I had to go to graphql, click on file, relativePath, eq (and type image file name next to it), childImageSharp, and src.
+// step 2 - I went to code exporter in graphql, selected StaticQuery hook, and copied graphql and everything between ``
+const query = graphql`
+    {
+      file(relativePath: {eq: "hero-img.png"}) {
+        childImageSharp {
+          fluid {
+             ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `
+
 const Hero = () => {
   return <h2>hero component</h2>
 }
