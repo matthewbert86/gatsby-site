d4b46ea385083823c3114911cc7f05c2b48d269e
Matt <matthewbertweb@gmail.com>
2020-07-09 14:54:23 -0400

    I added a new logo to the header. It is a filler logo, but I wanted to work on changing up the style a little bit to make it more unique from the starter template. I also have it set to take the user back to the home page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react"
-import logo from "../assets/logo.svg"
+import logo from "../assets/cropped-logo-2.jpg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
+import { Link } from "gatsby"
 const Navbar = () => {
   return <nav className="navbar">
     <div className="nav-center">
       <div className="nav-header">
-        <img src={logo} alt="logo" />
+        <Link to="/" className="logo-links">
+          <img src={logo} alt="logo" />
+            </Link>
+        
         <button type="button" className="toggle-btn">
           <FaAlignRight></FaAlignRight>
         </button>
