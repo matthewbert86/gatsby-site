1ff027be4da5964a19d477c4745076f5818d2a72
Matt <matthewbertweb@gmail.com>
2020-07-10 10:54:39 -0400

    I added the content to the About Me page, which was all pulled in from Strapi.

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,19 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>This page both exists and does not exist at the same time. Feel free to stay here and think on that for a while, or click the button below to get outta here!</h1>
+          <Link to="/" className="btn">
+            E.T. Phone Home!
+        </Link>
+        </div>
+      </main>
+    </Layout>
+    
+  )
 }
 
 export default Error
